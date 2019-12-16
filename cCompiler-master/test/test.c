2265081d87706f3e7e5b2d632ec261c97ac6ad5c
sunxiaobing <759677946@qq.com>
2019-12-16 21:36:47 +0800

    init

---
+++ cCompiler-master/test/test.c
@@ -0,0 +1,29 @@
+int fact(int n){
+	int temp;
+    if(n==1)
+        return n;
+    else{
+    	temp=(n*fact(n-1));
+    	return temp;
+    }
+}
+
+
+int main()
+{
+    int result,times;
+    int x[4];
+    times=read();
+    for(int i=0;i<times;i++){
+    	int m = read();
+    	if( m > 1) {
+        	result=fact(m);
+        }
+        else {
+            result = 1;
+        }
+        print(result);
+	}
+    return 0;
+}
+
