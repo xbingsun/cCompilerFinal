fecb4cdc15f5d013da173cea6968e4fb45d6703e
sunxiaobing <759677946@qq.com>
2019-12-18 21:46:41 +0800

    modify.

--- cCompiler-master/objectcode/result.asm
+++ cCompiler-master/objectcode/result.asm
@@ -23,100 +23,43 @@ print:
     syscall
     move $v0,$0
     jr $ra
-fact:
+main:
+	li $t1,3
+	move $t2,$t1
+	li $t1,2
+	move $t3,$t1
 	li $t1,1
-	beq $a0,$t1,label0
+	li $t4,2
+	slt $t5,$t1,$t4
+	li $t1,4
+	li $t4,3
+	slt $t6,$t1,$t4
+	and $t1,$t5,$t6
+	li $t4,3
+	li $t5,1
+	sgt $t6,$t4,$t5
+	add $t4,$t6,$t1
+	li $t1,0
+	bgt $t4,$t1,label0
 	j label1
 label0:
-	move $v0,$a0
-	jr $ra
-	j label2
-label1:
-	li $t1,1
-	sub $t2,$a0,$t1
 	move $t0,$a0
 	move $a0,$t2
-	addi $sp,$sp,-24
-	sw $t0,0($sp)
-	sw $ra,4($sp)
-	sw $t1,8($sp)
-	sw $t2,12($sp)
-	sw $t3,16($sp)
-	sw $t4,20($sp)
-	jal fact
-	lw $a0,0($sp)
-	lw $ra,4($sp)
-	lw $t1,8($sp)
-	lw $t2,12($sp)
-	lw $t3,16($sp)
-	lw $t4,20($sp)
-	addi $sp,$sp,24
-	move $t1 $v0
-	mul $t2,$a0,$t1
-	move $t1,$t2
-	move $v0,$t1
-	jr $ra
-label2:
-main:
-	addi $sp,$sp,-4
-	sw $ra,0($sp)
-	jal read
-	lw $ra,0($sp)
-	move $t2,$v0
-	addi $sp,$sp,4
-	move $t3,$t2
-	li $t2,0
-	move $t4,$t2
-label3:
-	blt $t4,$t3,label4
-	j label5
-label4:
-	addi $sp,$sp,-4
-	sw $ra,0($sp)
-	jal read
-	lw $ra,0($sp)
-	move $t2,$v0
-	addi $sp,$sp,4
-	move $t5,$t2
-	li $t2,1
-	bgt $t5,$t2,label6
-	j label7
-label6:
-	move $t0,$a0
-	move $a0,$t5
-	addi $sp,$sp,-24
-	sw $t0,0($sp)
-	sw $ra,4($sp)
-	sw $t1,8($sp)
-	sw $t2,12($sp)
-	sw $t3,16($sp)
-	sw $t4,20($sp)
-	jal fact
-	lw $a0,0($sp)
-	lw $ra,4($sp)
-	lw $t1,8($sp)
-	lw $t2,12($sp)
-	lw $t3,16($sp)
-	lw $t4,20($sp)
-	addi $sp,$sp,24
-	move $t2 $v0
-	move $t6,$t2
-	j label8
-label7:
-	li $t2,1
-	move $t6,$t2
-label8:
-	move $t0,$a0
-	move $a0,$t6
 	addi $sp,$sp,-4
 	sw $ra,0($sp)
 	jal print
 	lw $ra,0($sp)
 	addi $sp,$sp,4
-	li $t2,1
-	add $t4,$t4,$t2
-	j label3
-label5:
-	li $t2,0
-	move $v0,$t2
+	j label2
+label1:
+	move $t0,$a0
+	move $a0,$t3
+	addi $sp,$sp,-4
+	sw $ra,0($sp)
+	jal print
+	lw $ra,0($sp)
+	addi $sp,$sp,4
+label2:
+	li $t4,0
+	move $v0,$t4
 	jr $ra
