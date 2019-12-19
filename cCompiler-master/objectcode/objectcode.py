5f2a265241e99f16740b068ef968948d500c6c97
zyr <13516173041@163.com>
2019-12-19 14:07:35 +0800

    add array

--- cCompiler-master/objectcode/objectcode.py
+++ cCompiler-master/objectcode/objectcode.py
@@ -1,11 +1,13 @@
 import os
 import re
 
-regs = ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8',
-        't9', 's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7']
+regs = ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7']
 table = {}  # 存储变量与寄存器的对应关系
 reg_ok = {}  # 存储各个寄存器的使用情况
 variables = []  # 存储所有还未被寄存器替代的temp变量(临时变量)
+arrayDefinition = ['.align 2\n']  # .data代码段 分配数组空间
+arrayAssign = ''
+flag = 0  # 数组第一次调用
 
 
 def Load_Var(Inter):  # 找到所有须用寄存器替代的变量
@@ -27,7 +29,6 @@ def Load_Inter(filename):  # 分割生成的中间代码，将每行代码转成
         line = line.strip(')')
         line = line.replace(',', '')
         lines.append(line.split(' '))
-        print(lines)
     return lines
 
 
@@ -44,61 +45,164 @@ def Get_R(string):  # 为variables中的变量分配寄存器
             keys.append(key)
         for key in keys:  # 当遇到未分配寄存器的变量时，清空之前所有分配的临时变量的映射关系！！！
             if 'temp' in key and key not in variables:  #
-                print(table)
-                print("enter {}".format(key))
                 reg_ok[table[key]] = 1
                 del table[key]  # 将分配给此temp变量的寄存器收回
         for reg in regs:  # 对于所有寄存器
             if reg_ok[reg] == 1:  # 如果寄存器可用
                 table[string] = reg  # 将可用寄存器分配给该变量，映射关系存到table中
                 reg_ok[reg] = 0  # 寄存器reg设置为已用
+                if "array" in string:
+                    regs.remove(reg)  # array的寄存器永远不会被收回
+                    print(regs)
                 return '$' + reg
 
 
 def translate(line):
+    global arrayAssign, flag
+    # 数组操作
+    # 数组调用
+    # sw $v0 0($t0)  # 将$vo中的数放到t0对应的地址中
+
+    if "&array" in line[1]:
+        res = '\tadd {0},{1},{2}\n'.format(Get_R(line[3]), Get_R(line[1].replace("&", "")),
+                                           Get_R(line[2]))
+        return res
+    # 给数组元素赋值
+    if line[0] == "=" and "*temp" in line[3]:
+        res = '\tsw {} 0({})\n'.format(Get_R(line[1]), Get_R(line[3].replace("*", "")))
+        return res
+
+
+    # 定义数组
+    if line[0] == 'DEC':
+        res = '{}:.space 1024\n'.format(line[3])
+        arrayDefinition.append(res)
+        arrayAssign += '\tla {},{}\n'.format(Get_R(line[3]), line[3])
+        return arrayAssign
     if line[0] == 'LABEL':  # 转换 label
         return line[1] + ':'
-
-    if line[0] == '=': #无运算操作的赋值
+    if line[0] == '=':  # 无运算操作的赋值
         if line[1][0] == '#':  # 用常数赋值
             # 被赋值变量用分配的寄存器替换，使用li语句完成使用立即数赋值的功能
             return '\tli %s,%s' % (Get_R(line[3]), line[1].replace('#', ''))
         else:  # 用变量赋值
             # 分别找到两个变量所在寄存器，使用move语句实现赋值操作
-            return '\tmove %s,%s' % (Get_R(line[3]), Get_R(line[1]))
+            if "*" in line[1]:
+                res = '\tlw {},0({})'.format(Get_R(line[3]), Get_R(line[1].replace("*", "")))
+                return res
+            else:
+                return '\tmove %s,%s' % (Get_R(line[3]), Get_R(line[1]))
 
     if line[0] == '+':
-        return '\tadd %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tadd {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tadd {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tadd %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
     # sub $1,$2,$3  $1=$2-$3
     elif line[0] == '-':
-        return '\tsub %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tsub {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tsub {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tsub %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
     # mul $1,$2,$3  $1=$2*$3
     elif line[0] == '*':
-        return '\tmul %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tmul {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tmul {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tmul %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
     # sub $1,$2,$3  $1=$2/$3
     elif line[0] == '/':
-        return '\tdiv %s,%s\n\tmflo %s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tdiv {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tdiv {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tdiv %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
     # slt $1,$2,$3 if($2<$3)   $1=1 else $1=0
     elif line[0] == '<':
-        return '\tslt %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tslt {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tslt {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tslt %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
     elif line[0] == '>':
-        return '\tsgt %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tsgt {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tsgt {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tsgt %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
 
     elif line[0] == '&&' and line[3][0] == 't':
-        return '\tand %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tand {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tand {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                          Get_R(line[3]),
+                                                          Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tand %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
     elif line[0] == '||' and line[3][0] == 't':
-        return '\tor %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
-    elif line[0] == '&&' and line[3][0] == 'l':   #&& || 跳转如何处理
-        return '\tadd {4},{1},{0}\n\tli {3},2\n\tbeq {4},{3},{2}' .format(Get_R(line[1]), Get_R(line[2]), line[3], Get_R("2"), Get_R("tempand"))
+        if "*" in line[1]:
+            return '\tlw {},0({})\n\tor {},{},{}'.format(Get_R("temp*"), Get_R(line[1].replace("*", "")),
+                                                         Get_R(line[3]),
+                                                         Get_R("temp*"), Get_R(line[2]))
+        elif "*" in line[2]:
+            return '\tlw {},0({})\n\tor {},{},{}'.format(Get_R("temp*"), Get_R(line[2].replace("*", "")),
+                                                         Get_R(line[3]),
+                                                         Get_R("temp*"), Get_R(line[1]))
+        else:
+            return '\tor %s,%s,%s' % (Get_R(line[3]), Get_R(line[1]), Get_R(line[2]))
+    elif line[0] == '&&' and line[3][0] == 'l':  # && || 跳转如何处理
+        return '\tadd {4},{1},{0}\n\tli {3},2\n\tbeq {4},{3},{2}'.format(Get_R(line[1]), Get_R(line[2]), line[3],
+                                                                         Get_R("2"), Get_R("tempand"))
     elif line[0] == '||' and line[3][0] == 'l':
-        return '\tadd {4},{1},{0}\n\tli {3},0\n\tbgt {4},{3},{2}' .format(Get_R(line[1]), Get_R(line[2]), line[3], Get_R("0"), Get_R("tempor"))
+        return '\tadd {4},{1},{0}\n\tli {3},0\n\tbgt {4},{3},{2}'.format(Get_R(line[1]), Get_R(line[2]), line[3],
+                                                                         Get_R("0"), Get_R("tempor"))
 
-    if line[0] == 'CALL' and line[3] != '_': #函数调用并赋给变量
+    if line[0] == 'CALL' and line[3] != '_':  # 函数调用并赋给变量
         res = ''
-        if line[2] != '{}': # 传递函数参数，先把a0中数据转移到t0,再将当前参数放到a0中
+        if line[2] != '{}':  # 传递函数参数，先把a0中数据转移到t0,再将当前参数放到a0中
             arg = line[2].strip('}').strip('{')
             arglist = arg.split(';')
-            res += '\tmove $t0,$a0\n\tmove $a0,%s\n' % Get_R(arglist[0])
+            if "*" in arglist[0]:
+                # lw {},0({})'.format(Get_R(line[3]), Get_R(line[1].replace("*", "")
+                res += '\tmove $t0,$a0\n\tlw $a0,0({})\n'.format(Get_R(arglist[0].replace("*", "")))
+            else:
+                res += '\tmove $t0,$a0\n\tmove $a0,%s\n' % Get_R(arglist[0])
         if line[1] == 'read' or line[1] == 'print':  # read 函数 与 print函数的调用，插入提前写好的固定代码段
             # addi	$sp, $sp, -4	# allocate stack space # 栈中开辟1个新地址
             # sw ra, 0($sp)	 # to save return address SW(Store Word)用于将源寄存器中的值存入指定的地址
@@ -113,7 +217,7 @@ def translate(line):
                 line[1], Get_R(line[3]))
             return res
 
-    if line[0] == 'j' and line[1] == '_' and line[2] == '_' :  # 无条件跳转
+    if line[0] == 'j' and line[1] == '_' and line[2] == '_':  # 无条件跳转
         return '\tj %s' % line[3]
 
     if line[0] == 'RETURN':  # 函数调用结束，存储返回值，跳转回调用者位置
@@ -125,7 +229,7 @@ def translate(line):
         # bgt    $t0,$t1, target  # branch to target if  $t0 > $t1
         # bge    $t0,$t1, target  # branch to target if  $t0 >= $t1
         # 四元式 (jop, arg1, arg2, res)
-        if line[0] == 'j==': # ==
+        if line[0] == 'j==':  # ==
             return '\tbeq %s,%s,%s' % (Get_R(line[1]), Get_R(line[2]), line[3])
         if line[0] == 'j!=':
             return '\tbne %s,%s,%s' % (Get_R(line[1]), Get_R(line[2]), line[3])
@@ -139,18 +243,22 @@ def translate(line):
             return '\tble %s,%s,%s' % (Get_R(line[1]), Get_R(line[2]), line[3])
 
     if line[0] == 'FUNCTION':  # 转换函数标号
-        if line[2] != '{}': # 只将函数参数与a0寄存器关联，不生成汇编指令
+        if line[2] != '{}':  # 只将函数参数与a0寄存器关联，不生成汇编指令
             param = line[2].strip('}').strip('{')
             paramlist = param.split(';')
-            table[paramlist[0]] = 'a0' # 函数参数统一从a0取 (只能处理传递一个参数的情况)
+            table[paramlist[0]] = 'a0'  # 函数参数统一从a0取 (只能处理传递一个参数的情况)
         return '%s:' % line[1]
 
     if line[0] == 'CALL' and line[3] == '_':
         res = ''
-        if line[2] != '{}': # 传递函数参数，先把a0中数据转移到t0,再将当前参数放到a0中
+        if line[2] != '{}':  # 传递函数参数，先把a0中数据转移到t0,再将当前参数放到a0中
             arg = line[2].strip('}').strip('{')
             arglist = arg.split(';')
-            res += '\tmove $t0,$a0\n\tmove $a0,%s\n' % Get_R(arglist[0])
+            if "*" in arglist[0]:
+                # lw {},0({})'.format(Get_R(line[3]), Get_R(line[1].replace("*", "")
+                res += '\tmove $t0,$a0\n\tlw $a0,0({})\n'.format(Get_R(arglist[0].replace("*", "")))
+            else:
+                res += '\tmove $t0,$a0\n\tmove $a0,%s\n' % Get_R(arglist[0])
         if line[1] == 'read' or line[1] == 'print':
             res += '\taddi $sp,$sp,-4\n\tsw $ra,0($sp)\n\tjal %s\n\tlw $ra,0($sp)\n\taddi $sp,$sp,4' % (line[1])
             return res
@@ -158,18 +266,23 @@ def translate(line):
             res += '\taddi $sp,$sp,-24\n\tsw $t0,0($sp)\n\tsw $ra,4($sp)\n\tsw $t1,8($sp)\n\tsw $t2,12($sp)\n\tsw $t3,16($sp)\n\tsw $t4,20($sp)\n\tjal %s\n\tlw $a0,0($sp)\n\tlw $ra,4($sp)\n\tlw $t1,8($sp)\n\tlw $t2,12($sp)\n\tlw $t3,16($sp)\n\tlw $t4,20($sp)\n\taddi $sp,$sp,24' % (
                 line[1])
             return res
+
     return ''
 
 
 def write_to_txt(Obj):  # 将最终转换结果写到result.asm文件中
+    dataSection = ""
+    for line in arrayDefinition:
+        dataSection += line
     f = open('result.asm', 'w')
     # 统一使用开头：
     template = '''
 .data
 _prompt: .asciiz "Enter an integer:"
 _ret: .asciiz "\\n"
+''' + dataSection + '''
 .globl main
-.text
+.text''' + '''
 jal main
 li $v0,10
 syscall
@@ -201,8 +314,6 @@ def parser():  # 转换的程序入口
         reg_ok[reg] = 1  # 初始化，所有寄存器都可用
     Inter = Load_Inter('inter.txt')  # 读取中间代码
     Load_Var(Inter)  # 第一遍扫描，记录所有变量
-    print(Inter)
-    print(variables)
     Obj = []  # 存储最终转换结果
     for line in Inter:  # 逐行进行转换
         obj_line = translate(line)  # 转换中间代码成MIPS汇编
--- cCompiler-master/objectcode/result.asm
+++ cCompiler-master/objectcode/result.asm
@@ -2,6 +2,10 @@
 .data
 _prompt: .asciiz "Enter an integer:"
 _ret: .asciiz "\n"
+.align 2
+array0:.space 1024
+array1:.space 1024
+
 .globl main
 .text
 jal main
@@ -24,42 +28,63 @@ print:
     move $v0,$0
     jr $ra
 main:
-	li $t1,3
-	move $t2,$t1
-	li $t1,2
-	move $t3,$t1
-	li $t1,1
+	li $t1,10
+	li $t2,4
+	mul $t3,$t2,$t1
+	la $t1,array0
+
+	li $t2,5
+	li $t4,4
+	mul $t5,$t4,$t2
+	la $t1,array0
+	la $t2,array1
+
 	li $t4,2
-	slt $t5,$t1,$t4
-	li $t1,4
+	li $t6,4
+	mul $t7,$t4,$t6
+	add $t4,$t1,$t7
+
+	li $t6,5
+	sw $t6 0($t4)
+
 	li $t4,3
-	slt $t6,$t1,$t4
-	and $t1,$t5,$t6
+	li $t6,4
+	mul $t7,$t4,$t6
+	add $t4,$t2,$t7
+
+	li $t6,10
+	sw $t6 0($t4)
+
+	li $t4,2
+	li $t6,4
+	mul $t7,$t4,$t6
+	add $t4,$t1,$t7
+
+	li $t6,3
+	lw $t7,0($t4)
+	add $t4,$t7,$t6
+	move $t6,$t4
 	li $t4,3
-	li $t5,1
-	sgt $t6,$t4,$t5
-	add $t4,$t6,$t1
-	li $t1,0
-	bgt $t4,$t1,label0
+	li $t7,4
+	mul $t8,$t4,$t7
+	add $t4,$t2,$t8
+
+	li $t7,1
+	lw $t8,0($t4)
+	sub $t4,$t8,$t7
+	move $t7,$t4
+	blt $t6,$t7,label0
 	j label1
 label0:
+	li $t4,1
 	move $t0,$a0
-	move $a0,$t2
+	move $a0,$t4
 	addi $sp,$sp,-4
 	sw $ra,0($sp)
 	jal print
 	lw $ra,0($sp)
 	addi $sp,$sp,4
-	j label2
 label1:
-	move $t0,$a0
-	move $a0,$t3
-	addi $sp,$sp,-4
-	sw $ra,0($sp)
-	jal print
-	lw $ra,0($sp)
-	addi $sp,$sp,4
-label2:
 	li $t4,0
 	move $v0,$t4
 	jr $ra
